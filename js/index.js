59081732815b14e3b749d6b812790d6e4d6b2d21
maciej123m <maciejrybakxd2@gmail.com>
2020-01-28 17:12:35 +0100

    zmiany

--- js/index.js
+++ js/index.js
@@ -14,3 +14,11 @@ $(window).scroll(function(){
     if($(this).scrollTop()>300) $('.backToTop').fadeIn();
     else $('.backToTop').fadeOut();
 })
+
+function white()
+{
+    let el = document.getElementById( 'mainbox' );
+    if( el )
+    el.className = 'MainContentWhite';
+    document.body.style.background = "#f3f3f3"
+}
