3bab5e4782a17630a854a86db5581a007e8954fc
maciej123m <maciejrybakxd2@gmail.com>
2020-01-29 15:48:04 +0100

    działające motywy

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
 } */
 
 $(document).ready(function(){
+      //$('.backToTop').fadeOut()
       $(".backToTop").on('click', function () {
           $("html, body").animate({scrollTop: 0}, 400);
       });
@@ -17,8 +18,90 @@ $(window).scroll(function(){
 
 function white()
 {
-    let el = document.getElementById( 'mainbox' );
-    if( el )
-    el.className = 'MainContentWhite';
     document.body.style.background = "#f3f3f3"
+
+    const mainBox = document.getElementById("mainbox")
+    mainBox.style.background = "#4BABFF"
+    mainBox.style.color = "#000"
+
+    let a = document.querySelectorAll( '.navbar a' )
+    for(let i=0; i<a.length; i++ ) {
+      a[i].style.color = "#000"
+    }
+
+    a = document.querySelectorAll( '.header > h2' )
+    a[0].style.color = "#000"
+    
+    a = document.querySelectorAll( 'footer > p' )
+    a[0].style.color = "#000"
+
+    document.getElementById("fot").style.background = "#4BABFF"
+    setCookie("theme",1,365)
+
+}
+
+function black()
+{
+    document.body.style.background = "#12141a"
+
+    const mainBox = document.getElementById("mainbox")
+    mainBox.style.background = "#151d22"
+    mainBox.style.color = "#ddddee"
+
+
+    let a = document.querySelectorAll( '.navbar a' )
+    for(let i=0; i<a.length; i++ ) {
+      a[i].style.color = "#d9d9e4"
+    }
+
+    a = document.querySelectorAll( '.header > h2' )
+    a[0].style.color = "#e7e7f3"
+    
+    a = document.querySelectorAll( 'footer > p' )
+    a[0].style.color = "#c8c8d4"
+
+    document.getElementById("fot").style.background = "#151d22"
+
+    setCookie("theme",0,365)
+}
+
+window.onload = function() {
+    this.checkCookie()
+ }
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+    }
+
+function checkCookie() {
+    var theme = getCookie("theme");
+    if (theme != "") {
+        if(theme==1) {
+            white()
+        }
+        else {
+            black()
+        }
+    } else {
+        setCookie("theme", 0, 365);
+    }
 }
